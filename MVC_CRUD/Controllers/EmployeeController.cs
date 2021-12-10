777c669c80d892abb29619b3e918318cec9a9aff
DESKTOP-DOLPF4J\M4tiD <m4tid@live.com.ph>
2021-12-10 11:01:33 +0800

    Referred on this video 2017 tutorial: https://www.youtube.com/watch?v=_qIYBgWTlTo&ab_channel=CodAffection Issue 1: index.cshtml at line 25 has improper way of escaping its special character of @ I can't confirm if the link is actually working. I can't even see the X UI appearing in the Add New button upon running in chrome Issue 2: Can't popup dialog, index.cshtml line 54-71 dialog is not a function
    Added UI and its theme using datatable https://datatables.net/

---
+++ MVC_CRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using MVC_CRUD.Models;
+
+namespace MVC_CRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (DBModels db = new DBModels())
+            {
+                List<Employee> empList = db.Employees.ToList<Employee>();
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            return View(new Employee());
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit()
+        {
+            return View();
+        }
+    }
+}
